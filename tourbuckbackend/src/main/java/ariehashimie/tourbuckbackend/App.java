355b865308880ab53e3e4371a735b4962e167564
hairilhashimie <hairil.hashimie@gmail.com>
2018-08-17 03:30:24 +0800

    first phase are done

---
+++ tourbuckbackend/src/main/java/ariehashimie/tourbuckbackend/App.java
@@ -0,0 +1,13 @@
+package ariehashimie.tourbuckbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
